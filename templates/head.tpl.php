15d17faae306562b4b57c10fbbf2ef594c2f2636
Frank Matheron <frankmatheron@gmail.com>
2013-06-20 16:16:58 +0200

    remove colvis from header

--- templates/head.tpl.php
+++ templates/head.tpl.php
@@ -6,9 +6,7 @@ return
     '<link rel="stylesheet" href="'.yourls_site_url(false).'/user/plugins/'.$fr_plugin_dirname.'/style.css" type="text/css" media="screen" />'.
     '<link rel="stylesheet" href="'.yourls_site_url(false).'/user/plugins/'.$fr_plugin_dirname.'/datatables/css/jquery.dataTables.css" type="text/css" media="screen" />'.
     '<link rel="stylesheet" href="'.yourls_site_url(false).'/user/plugins/'.$fr_plugin_dirname.'/datatables/css/ColReorder.css" type="text/css" media="screen" />'.
-    '<link rel="stylesheet" href="'.yourls_site_url(false).'/user/plugins/'.$fr_plugin_dirname.'/datatables/css/ColVis.css" type="text/css" media="screen" />'.
     '<script type="text/javascript" src="'.yourls_site_url(false).'/user/plugins/'.$fr_plugin_dirname.'/datatables/js/jquery.dataTables.min.js"></script>'.
     '<script type="text/javascript" src="'.yourls_site_url(false).'/user/plugins/'.$fr_plugin_dirname.'/datatables/js/ColReorderWithResize.js"></script>'.
-    '<script type="text/javascript" src="'.yourls_site_url(false).'/user/plugins/'.$fr_plugin_dirname.'/datatables/js/ColVis.min.js"></script>'.
     '<script type="text/javascript" src="'.yourls_site_url(false).'/user/plugins/'.$fr_plugin_dirname.'/script.js"></script>';
 
